740d6748d7f56372b6ed881b3f18a02469026007
Daniel Popoola <iamuchihadaniel236@gmail.com>
2025-03-11 15:04:32 +0100

    minor changes to url patter and home page

--- spotify_oauth/urls.py
+++ spotify_oauth/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('logout/', views.logout, name='logout'),
-    path('login/', views.spotify_login, name='spotify_login'),
-    path('', views.home, name='home'),
+    path('', views.spotify_login, name='spotify_login'),
+    path('home/', views.home, name='home'),
     path('callback/', views.spotify_callback, name='spotify_oauth/callback/')
 ]
