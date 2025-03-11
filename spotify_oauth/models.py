7d9b8a9aac636f1abe7e339e0058feaa0486af52
Daniel Popoola <iamuchihadaniel236@gmail.com>
2025-03-11 13:36:13 +0100

    to store tokens

--- spotify_oauth/models.py
+++ spotify_oauth/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class SpotifyToken(models.Model):
+    spotify_user_id = models.CharField(max_length=150, unique=True)
+    access_token = models.TextField()
+    refresh_token = models.TextField()
+    expires_at = models.IntegerField()
+    token_type = models.CharField(max_length=50, default='Bearer')
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.spotify_user_id}'s Spotify Token"
