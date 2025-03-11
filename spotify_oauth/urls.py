4a911d461774d5a47cbb17151b36bf6bcc001c87
Daniel Popoola <iamuchihadaniel236@gmail.com>
2025-03-11 13:36:27 +0100

    added url paths

--- spotify_oauth/urls.py
+++ spotify_oauth/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('login/', views.spotify_login, name='spotify_login'),
+    path('', views.home, name='home'),
     path('callback', views.spotify_callback, name='spotify_callback')
 ]
