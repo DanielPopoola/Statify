95c143fd43c65b3b3dc8f1585e87fdd59be7e2ff
Daniel Popoola <iamuchihadaniel236@gmail.com>
2025-03-19 00:20:41 +0100

    minor changes

--- spotify_dashboard/settings.py
+++ spotify_dashboard/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,12 +24,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure--&et3n2l(zon=0+x^-n_f@eu87*j5y@e8p*f!7hviu3m&02ajo'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['statify-hazel.vercel.app', 'localhost', '127.0.0.1']  
 
 
 # Application definition
